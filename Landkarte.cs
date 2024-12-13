5d6d9925a77ef8a85e631b0d8c452a0e8327a72e
Blum, Florian <florian.blum@sew-eurodrive.de>
2024-12-13 11:22:29 +0100

    added "Landkarte.cs"

---
+++ Landkarte.cs
@@ -0,0 +1,30 @@
+
+
+public class Landkarte
+{
+
+  public BahnElement[][] karte;
+
+
+  public Landkarte()
+  {
+
+  }
+
+  public void getKarteAusDatei()
+  {
+
+  }
+
+  public uint[][] getStationen()
+  {
+
+    return null;
+  }
+
+  public uint[][] erstelleWeg(uint[][] ziele)
+  {
+
+    return null;
+  }
+};
